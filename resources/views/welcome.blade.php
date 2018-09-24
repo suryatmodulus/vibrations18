a92caf1a531a078069a4685107a8a3bb3a84213f
modulus <suryatmodulus@gmail.com>
2018-09-24 18:13:04 +0530

    small changes to ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="row text-center">
         <div class="col center-block">
             <img class="img-logo img-responsive center-block" src="{{ asset('images/full_logo.png') }}" alt="">
-            <a href="{{route('registration')}}"><button class="register-btn">Register Now</button></a>
+            <a href="{{route('registration')}}"><button class="register-btn mb-3">Register Now</button></a>
         </div>
     </div>
 </div>
